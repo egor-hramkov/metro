49787299c826c9bc5ae69a69bfee93538208b809
egor-hramkov <egor_kuplinov@mail.ru>
2024-02-16 19:36:12 +0400

    add pprint

--- lab2.py
+++ lab2.py
@@ -1,6 +1,7 @@
 import time
 
 import psutil
+import pprint as pp
 
 current, min_freq, max_freq = psutil.cpu_freq()
 n = 10
@@ -9,7 +10,8 @@ for i in range(n):
     freq = psutil.cpu_freq().current
     freq_list.append(freq)
     time.sleep(0.2)
-print(f"При {n=} следующие результаты:", freq_list)
+#print(f"При {n=} следующие результаты:", freq_list)
+print(f"При {n=} следующие результаты:", pp.pformat(freq_list))
 
 n = 100
 freq_list = []
@@ -17,7 +19,7 @@ for i in range(n):
     freq = psutil.cpu_freq().current
     freq_list.append(freq)
     time.sleep(0.02)
-print(f"При {n=} следующие результаты:", freq_list)
+print(f"При {n=} следующие результаты:", pp.pformat(freq_list))
 
 n = 1000
 freq_list = []
@@ -25,4 +27,4 @@ for i in range(n):
     freq = psutil.cpu_freq().current
     freq_list.append(freq)
     time.sleep(0.002)
-print(f"При {n=} следующие результаты:", freq_list)
+print(f"При {n=} следующие результаты:", pp.pformat(freq_list))
